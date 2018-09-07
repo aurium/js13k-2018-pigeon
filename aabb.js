1c3f9ab0646584ea6149fa278d41f33b36e44543
Aurélio A. Heckert <aurium@colivre.coop.br>
2018-09-07 01:22:44 -0300

    Add more information about colision

--- aabb.js
+++ aabb.js
@@ -22,8 +22,8 @@ AFRAME.registerComponent('aabb-collider', {
   init: function () {
     this.els = [];
     this.collisions = [];
-    this.elMax = new THREE.Vector3();
-    this.elMin = new THREE.Vector3();
+    this.el.bbMax = new THREE.Vector3();
+    this.el.bbMin = new THREE.Vector3();
   },
 
   /**
@@ -49,7 +49,7 @@ AFRAME.registerComponent('aabb-collider', {
     return function () {
       var collisions = [];
       var el = this.el;
-      var mesh = el.getObject3D('mesh');
+      var mesh = el.mesh = el.getObject3D('mesh');
       var self = this;
       // No mesh, no collisions
       if (!mesh) { return; }
@@ -81,15 +81,15 @@ AFRAME.registerComponent('aabb-collider', {
         var elMax;
         if (!mesh) { return; }
         boundingBox.setFromObject(mesh);
-        elMin = boundingBox.min;
-        elMax = boundingBox.max;
+        elMin = el.bbMin = boundingBox.min;
+        elMax = el.bbMax = boundingBox.max;
         // Bounding boxes are always aligned with the world coordinate system.
         // The collision test checks for the conditions where cubes intersect.
         // It's an extension to 3 dimensions of this approach (with the condition negated)
         // https://www.youtube.com/watch?v=ghqD3e37R7E
-        intersected = (self.elMin.x <= elMax.x && self.elMax.x >= elMin.x) &&
-                      (self.elMin.y <= elMax.y && self.elMax.y >= elMin.y) &&
-                      (self.elMin.z <= elMax.z && self.elMax.z >= elMin.z);
+        intersected = (self.el.bbMin.x <= elMax.x && self.el.bbMax.x >= elMin.x) &&
+                      (self.el.bbMin.y <= elMax.y && self.el.bbMax.y >= elMin.y) &&
+                      (self.el.bbMin.z <= elMax.z && self.el.bbMax.z >= elMin.z);
         if (!intersected) { return; }
         collisions.push(el);
       }
@@ -103,8 +103,8 @@ AFRAME.registerComponent('aabb-collider', {
 
       function updateBoundingBox () {
         boundingBox.setFromObject(mesh);
-        self.elMin.copy(boundingBox.min);
-        self.elMax.copy(boundingBox.max);
+        self.el.bbMin.copy(boundingBox.min);
+        self.el.bbMax.copy(boundingBox.max);
       }
     };
   })()
